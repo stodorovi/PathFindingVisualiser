d6cf2fff6af6a57075c8be5b0ea6156ad1e44399
Sebastijan Todorović <todorovicsebastijan@gmail.com>
2021-12-01 20:15:23 +0100

    Added constructors

--- PathfindingAlgorithms/Types.h
+++ PathfindingAlgorithms/Types.h
@@ -19,6 +19,12 @@ namespace pathAlgs {
         int x;
         int y;
 
+        Point() : x{0}, y{0}
+        {}
+
+        Point(int x, int y) : x{x}, y{y}
+        {}
+
         bool operator==(Point other) const {
 
             return x == other.x
